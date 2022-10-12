7663f5b566aa142566dd92bc5ed6a13c9e2575b4
Allaudeen John <Allaudeen_John@ad.Infosys.com>
2022-10-13 03:22:41 +0530

    Refactored the matrix print logic and tested

--- asml-assignment/src/com/asml/assignment/MatrixNavigation.java
+++ asml-assignment/src/com/asml/assignment/MatrixNavigation.java
@@ -21,85 +21,91 @@ public class MatrixNavigation {
 	 * @return returnArray A single dimensional array which holds the navigated input array. 
 	 */
 	public int[] printSpiralMatrix(int[][] inputMatrix) {
-		int totalRows = 0;
-		int totalCols = 0;
-		if (inputMatrix == null) {
-			throw new IllegalArgumentException("The input matrix provided is not valid!");
-		} else {
-			totalRows = Long.valueOf(Arrays.stream(inputMatrix).count()).intValue();
-			totalCols = Long.valueOf(Arrays.stream(inputMatrix[0]).count()).intValue();
-		}
+        int totalRows = 0;
+        int totalCols = 0;
+        if (inputMatrix == null) {
+            throw new IllegalArgumentException("The input matrix provided is not valid!");
+        } else {
+            totalRows = Long.valueOf(Arrays.stream(inputMatrix).count()).intValue();
+            totalCols = Long.valueOf(Arrays.stream(inputMatrix[0]).count()).intValue();
+        }
 
-		int totalElementSize = totalRows * totalCols;
-		int elementsCounter = 0;
-		int startColId = 0;
-		int endColId = totalCols - 1;
-		int startRowId = 0;
-		int endRowId = totalRows - 1;
-		int colIndex = 0;
-		int rowIndex = 0;
-		boolean isBottomRowPrinted = false;
-		int[] returnArray = new int[totalElementSize];
-		// Iterating the 2D array element by element till the last element exists
-		while (elementsCounter < totalElementSize) {
-			// Printing the first row of the matrix left to right
-			for (; colIndex <= endColId; colIndex++) {
-				System.out.print(inputMatrix[startRowId][colIndex] + " ");
-				returnArray[elementsCounter] = inputMatrix[startRowId][colIndex];
-				elementsCounter++;
-			}
-			startColId = colIndex - 1;
-			colIndex--;
-			endRowId = totalRows - 1;
-			totalRows--;
-			rowIndex = startRowId + 1;
+        int totalElementSize = totalRows * totalCols;
+        int[] returnArray = new int[totalElementSize];
+        totalCols--;
+        totalRows--;
+        int elementsCounter = 0;
+        int startColId = 0;
+        int startRowId = 0;
+        int endColId = totalCols;
+        int endRowId = totalRows;
+        int colIndex = 0;
+        int rowIndex = 0;
 
-			// Printing the right side of the matrix top to bottom
-			for (; rowIndex <= endRowId; rowIndex++) {
-				System.out.print(inputMatrix[rowIndex][startColId] + " ");
-				returnArray[elementsCounter] = inputMatrix[rowIndex][startColId];
-				elementsCounter++;
-			}
-			if (!isBottomRowPrinted) {
-				startRowId = rowIndex--;
-				totalCols--;
-				colIndex = startColId - 1;
-				endColId = totalCols - startColId;
-			} else {
-				startRowId = rowIndex;
-				totalCols--;
-				colIndex = startColId;
-				endColId = totalCols;
-			}
+        //Iterating the 2D array element by element
+        while (elementsCounter < totalElementSize) {
 
-			// Printing the bottom side of the matrix elements from right to left
-			if (totalRows > 0) {
-				for (; colIndex >= endColId; colIndex--) {
-					System.out.print(inputMatrix[rowIndex][colIndex] + " ");
-					returnArray[elementsCounter] = inputMatrix[rowIndex][colIndex];
-					elementsCounter++;
-					isBottomRowPrinted = true;
-				}
+            // Printing top left to right
+            for (colIndex = startColId; colIndex <= endColId; colIndex++) {
+                System.out.print(inputMatrix[startRowId][colIndex] + " ");
+                returnArray[elementsCounter] = inputMatrix[startRowId][colIndex];
+                elementsCounter++;
+            }
+            if(elementsCounter >= totalElementSize)break;
 
-				totalRows--;
-				startRowId = rowIndex--;
-				startColId = colIndex + 1;
-				endRowId = startRowId - totalRows;
-				endColId = colIndex + 1;
+            endColId = totalCols;
+            rowIndex += 1;
+            startRowId = totalRows - rowIndex;
+            startColId = colIndex - 1;
 
-				// Print the left side of the matrix from bottom to up
-				for (; rowIndex >= endRowId; rowIndex--) {
-					System.out.print(inputMatrix[rowIndex][startColId] + " ");
-					returnArray[elementsCounter] = inputMatrix[rowIndex][startColId];
-					elementsCounter++;
-				}
-				totalCols--;
-				startRowId = endRowId;
-				startColId = endColId + 1;
-				colIndex = startColId;
-				endColId = totalCols;
-			}
-		}
-		return returnArray;
-	}
+            //Printing right top to bottom
+           for (; rowIndex <= endRowId; rowIndex++) {
+                System.out.print(inputMatrix[rowIndex][startColId] + " ");
+                returnArray[elementsCounter] = inputMatrix[rowIndex][startColId];
+                elementsCounter++;
+            }
+            if(elementsCounter >= totalElementSize)break;
+
+            startRowId = endRowId;
+            endColId = startColId - totalCols;
+            colIndex = startColId - 1;
+            rowIndex = endRowId;
+
+            //Printing bottom right to left
+            for (; colIndex >= endColId; colIndex--) {
+                System.out.print(inputMatrix[rowIndex][colIndex] + " ");
+                returnArray[elementsCounter] = inputMatrix[rowIndex][colIndex];
+                elementsCounter++;
+            }
+            if(elementsCounter >= totalElementSize)break;
+
+            colIndex += 1;
+            endRowId = startRowId - totalRows;
+            rowIndex -= 1;
+            startColId = colIndex;
+            startRowId = rowIndex;
+            endColId = totalCols - startColId;
+            endRowId = totalRows - startRowId;
+            totalCols -= 1;
+            totalRows -= 1;
+
+            //Printing left bottom to top
+            for (; rowIndex >= endRowId; rowIndex--) {
+                System.out.print(inputMatrix[rowIndex][colIndex] + " ");
+                returnArray[elementsCounter] = inputMatrix[rowIndex][colIndex];
+                elementsCounter++;
+            }
+            if(elementsCounter >= totalElementSize)break;
+
+            startColId += 1;
+            startRowId = endRowId;
+            endColId = totalCols;
+            endRowId = totalRows;
+            colIndex = startColId;
+            rowIndex = startRowId;
+            totalCols -= 1;
+            totalRows -= 1;
+        }
+        return returnArray;
+    }
 }
